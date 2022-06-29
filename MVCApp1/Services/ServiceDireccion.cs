6c79327a5a7b2a17d308b6e5bf0353e8d0133566
vidapogosoft <vidapogosoft@hotmail.com>
2022-06-28 19:44:19 -0500

    partial view get
    services para HTTP Get

---
+++ MVCApp1/Services/ServiceDireccion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using MVCApp1.Models;
+
+namespace MVCApp1.Services
+{
+    public class ServiceDireccion
+    {
+        //la implementacion del HTT Get
+
+        List<Direcciones> direccion;
+        HttpClient _Client;
+
+        public ServiceDireccion()
+        {
+            _Client = new HttpClient();
+        }
+
+        public  List<Direcciones> GetDireccionesByIdRegistrado(string IdRegistrado)
+        {
+            var uriget = new Uri("http://localhost:3849/api/Registrado/" + IdRegistrado);
+
+
+            var responseget = _Client.GetAsync(uriget).Result;
+
+            if (responseget.IsSuccessStatusCode)
+            {
+                var contentget = responseget.Content.ReadAsStringAsync();
+
+                direccion = JsonConvert.DeserializeObject<List<Direcciones>>(contentget);
+
+            }
+
+            return direccion;
+        }
+
+    }
+}
