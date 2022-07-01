3262ffb41bb15c7190cf414ea979b02406145f13
vidapogosoft <vidapogosoft@hotmail.com>
2022-06-30 23:34:30 -0500

    taller razor pages
    get y post

--- RazorPages1/Pages/Registrado/Create.cshtml.cs
+++ RazorPages1/Pages/Registrado/Create.cshtml.cs
@@ -5,12 +5,63 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+
 namespace RazorPages1.Pages.Registrado
 {
     public class CreateModel : PageModel
     {
+
+        string BaseUrl = "http://localhost:3849/";
+
+        [BindProperty]
+        public Models.Registrado register { get; set; }
+
         public void OnGet()
         {
         }
+
+        //Post
+        public IActionResult OnPostAsync()
+        {
+            try
+            {
+                //Llamado hacia mis apis
+                using (var client = new HttpClient())
+                {
+
+                    client.BaseAddress = new Uri(BaseUrl);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    var json = JsonConvert.SerializeObject(register);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage Res = client.PostAsync("api/Registrado", content).GetAwaiter().GetResult();
+
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        var ResponsePost = Res.Content.ReadAsStringAsync().Result;
+
+                    }
+
+                 }
+
+
+                return RedirectToPage("/Registrado/Index");
+
+            }
+            catch
+            {
+                return Page();
+            }
+
+        }
+
     }
 }
--- RazorPages1/Pages/Registrado/Index.cshtml.cs
+++ RazorPages1/Pages/Registrado/Index.cshtml.cs
@@ -5,16 +5,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using Newtonsoft.Json;
+using RazorPages1.Models;
+
 namespace RazorPages1.Pages.Registrado
 {
     public class IndexModel : PageModel
     {
+        string BaseUrl = "http://localhost:3849/";
         public IEnumerable<Models.Registrado> ListRegistrados { get; set; }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
             //Llamado hacia mis apis
+            using (var client = new HttpClient())
+            {
 
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage Res = await client.GetAsync("api/Registrado/");
+
+                if(Res.IsSuccessStatusCode)
+                {
+
+                    var Response = Res.Content.ReadAsStringAsync().Result;
+
+                    ListRegistrados = JsonConvert.DeserializeObject<List<Models.Registrado>>(Response);
+
+                }
+
+            }
+
+            return Page();
         }
     }
 }
