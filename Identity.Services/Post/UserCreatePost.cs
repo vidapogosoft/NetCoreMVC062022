a60c0c1d104185c4b970aad1f03712d9f077239c
vidapogosoft <vidapogosoft@hotmail.com>
2022-07-05 22:09:40 -0500

    estructura de MediatR

---
+++ Identity.Services/Post/UserCreatePost.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MediatR;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Services.Post
+{
+    public class UserCreatePost : IRequest<IdentityResult>
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Identity.Services/Response/IdentityAccess.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Identity.Services.Response
+{
+    public class IdentityAccess
+    {
+        public bool Succeeded { get; set; }
+        public string AccessToken { get; set; }
+
+    }
+}
