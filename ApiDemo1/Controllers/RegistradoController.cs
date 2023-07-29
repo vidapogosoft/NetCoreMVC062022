eac08f1d4abcc6854b3148dcd97c1881fe908ed8
vidapogosoft <vidapogosoft@hotmail.com>
2023-07-29 08:27:18 -0500

    Update RegistradoController.cs

--- ApiDemo1/Controllers/RegistradoController.cs
+++ ApiDemo1/Controllers/RegistradoController.cs
@@ -13,7 +13,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace ApiDemo1.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class RegistradoController : ControllerBase
